052b796aa5ef60753732306508582f677234c381
MyukaQA <mylian214@gmail.com>
2021-12-17 10:02:07 +0700

    database, model forum

---
+++ app/Models/Forum.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Forum extends Model
+{
+    use HasFactory;
+
+    protected $table = "forum";
+    protected $guarded = ['id'];
+
+    public function get_img_forum(){
+        if(!$this->image){
+            return asset('img/umum/default.png');
+        }
+
+        return asset('img/forum/'.$this->image);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function komentar(){
+        return $this->hasMany(KomentarForum::class);
+    }
+}
---
+++ database/migrations/2021_12_17_025353_create_komentar_forum_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKomentarForumTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('komentar_forum', function (Blueprint $table) {
+            $table->id();
+            $table->string('desc');
+            $table->integer('user_id');
+            $table->integer('forum_id');
+            $table->integer('parent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('komentar_forum');
+    }
+}
