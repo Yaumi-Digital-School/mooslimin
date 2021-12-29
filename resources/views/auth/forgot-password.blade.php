311e2176ed2f5415ea446a58b84dfa530e373ddf
annissakamilam <kamilaannissa@ymail.com>
2021-12-29 19:34:13 +0700

    fix update forgot pass

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -3,9 +3,33 @@
     Muslim Tool
     @endsection
 
+
+
+
+
+
+
+
+
+
+    
+    @if(!isset($status))
+    <div class="h-screen w-screen flex flex-col justify-center items-center font-inter">
+        <img src="/img/umum/Envelope.png">
+        <div class="mt-10 mb-7">
+            <p class="font-inter font-bold text-3xl">Verifikasi Akun Anda</p>
+        </div>
+        <div class="text-center mb-6">
+            <p>Kami telah mengirim email permohonan ubah password ke email </p>
+            <p>Jika tidak terkirim klik  tombol dibawah ini.</p>
+        </div>
+        <button type="submit" class="w-64 text-base mb-5 px-6 py-2 mt-8 text-white bg-primary-500 rounded-lg hover:bg-primary-900">Kirim ulang email verifikasi</button>
+       
+    </div>        
+    @else
     <div class="h-screen w-screen flex flex-col justify-center  font-inter">
         <form method="POST" action="{{ route('reset.password.store') }}" class="mx-auto w-96">
-            {{ csrf_field() }}
+            @csrf
             <h1 class="text-3xl text-center font-bold">Lupa Password</h1>
 
            <div class="mt-20">
@@ -31,7 +55,11 @@
 
             <button type="submit" class="w-full text-base mb-5 px-6 py-2 mt-8 text-white bg-primary-500 rounded-lg hover:bg-primary-900">Request Password</button>
         </form>
+        
     </div>
+    @endif
+
+    
     {{-- <x-auth-card>
         <x-slot name="logo">
             <a href="/">
