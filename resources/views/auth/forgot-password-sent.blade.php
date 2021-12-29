100f10a88ad37667ce50037674f960e3fc89cea1
annissakamilam <kamilaannissa@ymail.com>
2021-12-29 14:44:13 +0700

    perbaikan forgot

--- resources/views/auth/forgot-password-sent.blade.php
+++ resources/views/auth/forgot-password-sent.blade.php
@@ -5,7 +5,9 @@
 
     <div class="h-screen w-screen flex flex-col justify-center font-inter">
         <div class="items-center">
-
+            <div class="space-x-1">
+                <img src="/img/umum/Envelope.png">
+            </div>
         </div>
     </div>
     
