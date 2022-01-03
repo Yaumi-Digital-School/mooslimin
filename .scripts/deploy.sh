40bdcbb2f8e3b8effd5af27cdd2f2b48e3412d2d
Yumna Pratista <yumnapratista24@192.168.1.30>
2022-01-03 14:52:41 +0700

    ci: add deployment script

---
+++ .scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Enter maintenance mode
+(php artisan down) || true
+
+# Pull latest
+git pull origin main
+
+# Install composer dependencies
+export COMPOSER_ALLOW_SUPERUSER=1
+composer dump-autoload
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Run database migrations
+php artisan migrate --force
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+
+# Compile npm assets
+npm cache clean --force
+npm install
+npm run prod
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment finished!"
